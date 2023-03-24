5a7af73ee46e013b76a141b0a81de3ff84b4c690
Qspiders-Wakad <Qspiders-Wakad@DESKTOP-AIGVO6J>
2023-03-24 10:53:32 +0530

    commit

---
+++ wcsm8seleniumproject/src/methodsOfWebDriver/QuitMethod.java
@@ -0,0 +1,30 @@
+package methodsOfWebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class QuitMethod {
+
+	// is use to close parent & child Browser!!!
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+
+		WebDriver driver=new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.get("http://omayo.blogspot.com/");
+
+		driver.findElement(By.xpath("//a[.='Open a popup window']")).click();
+
+		Thread.sleep(2000);
+
+		driver.quit();
+	}
+
+}
