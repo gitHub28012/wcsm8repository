e73600e0804b9a270e7a75b177b26ef3baff532e
Qspiders-Wakad <Qspiders-Wakad@DESKTOP-AIGVO6J>
2023-03-25 10:59:16 +0530

    commit

--- wcsm8seleniumproject/src/qsp/LauchFirefoxBrowser.java
+++ wcsm8seleniumproject/src/qsp/LauchFirefoxBrowser.java
@@ -7,7 +7,7 @@ public class LauchFirefoxBrowser {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		//System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
