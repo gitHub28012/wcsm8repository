f03d44af3e1409f25604a68687eb9f6e79cf237a
Qspiders-Wakad <Qspiders-Wakad@DESKTOP-AIGVO6J>
2023-04-05 11:06:14 +0530

    commit

---
+++ wcsm8seleniumproject/src/xpath/Xpath2.java
@@ -0,0 +1,32 @@
+package xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Xpath2 {
+	
+	// by using text() function
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		co.addArguments("--disable-notifications");
+		
+		WebDriver driver=new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.get("https://in.puma.com/in/en");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Men']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//span[text()='PUMA White-PUMA Red-PUMA Gold']")).click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
